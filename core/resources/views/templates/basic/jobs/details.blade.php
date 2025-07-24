bf856be4ca56560d6a699c44174afdefb3bbdb61
Md. Moniruzzaman (Office) <moon.mn717@gmail.com>
2025-07-24 14:36:17 +0600

    created all jobs and job details page

---
+++ core/resources/views/templates/basic/jobs/details.blade.php
@@ -0,0 +1,7 @@
+@extends($activeTemplate . 'layouts.frontend')
+@section('content')
+    @include($activeTemplate . 'partials.breadcrumb')
+    <section class="exam-section ptb-80">
+        this is a job details page.
+    </section>
+@endsection
