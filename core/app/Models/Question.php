76f4790324350a41c8e8008b3a6b75b406f78967
Md. Moniruzzaman (Home) <moon.mn717@gmail.com>
2025-07-25 00:09:16 +0600

    crate model mehod

--- core/app/Models/Question.php
+++ core/app/Models/Question.php
@@ -9,7 +9,7 @@ class Question extends Model
 {
     use HasFactory;
     protected $guarded = ['id'];
-    
+
     public function topics()
     {
         return $this->belongsToMany(Topic::class)->withTimestamps();
@@ -23,6 +23,42 @@ class Question extends Model
     public function questionBanks()
     {
         return $this->belongsToMany(QuestionBank::class)->withTimestamps();
+    }
+    /**
+     * Store a new question with its relations.
+     *
+     * @param array $data
+     * @param int $adminId
+     * @return self
+     */
+    public static function storeWithRelations(array $data, $adminId): self
+    {
+        // Create the question
+        $question = self::create([
+            'question_text'   => $data['question_text'],
+            'options'         => $data['options'],
+            'correct_answer'  => $data['correct_answer'],
+            'explanation'     => $data['explanation'] ?? null,
+            'created_by'      => $adminId,
+            'updated_by'      => $adminId,
+        ]);
+
+        // Attach topics if provided
+        if (!empty($data['topic_ids'])) {
+            $question->topics()->attach($data['topic_ids'], ['created_by' => $adminId]);
+        }
+
+        // Attach chapters if provided
+        if (!empty($data['chapter_ids'])) {
+            $question->chapters()->attach($data['chapter_ids'], ['created_by' => $adminId]);
+        }
+
+        // Attach question banks if provided
+        if (!empty($data['question_bank_ids'])) {
+            $question->questionBanks()->attach($data['question_bank_ids'], ['created_by' => $adminId]);
+        }
+
+        return $question;
     }
 
 }
