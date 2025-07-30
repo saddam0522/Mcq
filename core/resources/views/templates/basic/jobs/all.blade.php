c4dfae2d22813d51fb48bdbcc9bdff302da68896
Amit <amitmohammad46@gmail.com>
2025-07-30 15:26:58 +0600

    job list page design

--- core/resources/views/templates/basic/jobs/all.blade.php
+++ core/resources/views/templates/basic/jobs/all.blade.php
@@ -1,9 +1,25 @@
 @extends($activeTemplate . 'layouts.frontend')
 @section('content')
-    @include($activeTemplate . 'partials.breadcrumb')
-    <section class="exam-section ptb-80">
-        <div class="container">
-            this is a all job page.
+@include($activeTemplate . 'partials.breadcrumb')
+<section class="exam-section pt-20 pb-50">
+    <div class="container">
+        <div class="row job-cards g-4">
+            <div class="col-md-6 col-lg-4">
+                @include('templates.basic.sections.job_card')
+            </div>
+            <div class="col-md-6 col-lg-4">
+                @include('templates.basic.sections.job_card')
+            </div>
+            <div class="col-md-6 col-lg-4">
+                @include('templates.basic.sections.job_card')
+            </div>
+            <div class="col-md-6 col-lg-4">
+                @include('templates.basic.sections.job_card')
+            </div>
+            <div class="col-md-6 col-lg-4">
+                @include('templates.basic.sections.job_card')
+            </div>
         </div>
-    </section>
+    </div>
+</section>
 @endsection
